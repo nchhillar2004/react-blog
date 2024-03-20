49d16bb766805802f47f5675fb4dc7023df88d6c
Nishant Chhillar <nchhillar2004@gmail.com>
2024-03-20 22:25:13 +0530

    final commit

--- src/data/projects.js
+++ src/data/projects.js
@@ -10,7 +10,7 @@ export const projects = [
     {
         title: "Dry Fruits Ecommerce (Next.js)",
         content: "Designed and developed a Dry Fruits Ecommerce Website using Next.js with product browsing and checkout features.",
-        image: "",
+        image: "https://github.com/nchhillar2004/react-blog/blob/main/src/assets/rdf.png?raw=true",
         read: "",
         time: "9:15",
         date: "20/03/2024",
@@ -18,7 +18,7 @@ export const projects = [
     {
         title: "Financial Services (Next.js)",
         content: "Built a Financial Services Website using Next.js with account management and transaction tracking functionalities.",
-        image: "",
+        image: "https://github.com/nchhillar2004/react-blog/blob/main/src/assets/bhfs.png?raw=true",
         read: "",
         time: "9:30",
         date: "20/03/2024",
@@ -26,7 +26,7 @@ export const projects = [
     {
         title: "Wikipedia Clone (React.js)",
         content: "Created a Wikipedia Clone using React.js with search functionality and article display features.",
-        image: "",
+        image: "https://github.com/nchhillar2004/react-blog/blob/main/src/assets/wiki.png?raw=true",
         read: "",
         time: "9:45",
         date: "20/03/2024",
